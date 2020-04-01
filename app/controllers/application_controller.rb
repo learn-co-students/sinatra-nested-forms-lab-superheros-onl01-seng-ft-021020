a7ad957f8687c862a2654544343e6a8aa7e54d25
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-01 21:04:11 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,33 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/' do
+redirect to '/super_hero'
+  end
+  
+  get '/super_hero' do
+    erb :super_hero
+  end
+  
+  
+  
+  
+get '/teams' do
+ binding.pry
+      @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+      members = params[:team][:members]
 
+      members.each do |member_params|
+        SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
+      end
+      @super_heroes = SuperHero.all
+
+      erb :teams
+    end
+
+  
 end
